645d734a92cd5ef8d876808998e28c49d47c0ab4
austinp <auspendleton@gmail.com>
2024-04-10 16:09:37 -0600

    Port to 3000

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { errorHandler } = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 require("dotenv").config();
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
